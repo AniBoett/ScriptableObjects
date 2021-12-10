2bdbb394ae555ca5b6e4800cc57a57119bb36536
Anika Boettcher <10945351@my.uvu.edu>
2021-12-09 20:44:11 -0700

    Character scripts can attach to objects

--- ScriptableObjects/Assets/Scripts/CharacterAdd.cs
+++ ScriptableObjects/Assets/Scripts/CharacterAdd.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CharacterAdd : MonoBehaviour
 {
-    public Character character;
+    public Characters character;
     // Start is called before the first frame update
     void Start()
     {
